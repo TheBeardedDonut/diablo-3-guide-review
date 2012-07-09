3178da08c6e894ff10231e4cf78e39af563b48a0
Chris Payne <cdp.advertising@gmail.com>
2012-07-09 18:52:38 -0400

    Advanced forums added. Some css problems fixed.

--- themes/bartik/templates/page.tpl.php
+++ themes/bartik/templates/page.tpl.php
@@ -173,10 +173,6 @@
 
   <div id="main-wrapper" class="clearfix"><div id="main" class="clearfix">
 
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>
-
     <?php if ($page['sidebar_first']): ?>
       <div id="sidebar-first" class="column sidebar"><div class="section">
         <?php print render($page['sidebar_first']); ?>
